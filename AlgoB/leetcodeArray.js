37df19d33dfb75b920910a5b34ce6fce72847482
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-06-20 20:32:16 -0700

    modify syntax

--- AlgoB/leetcodeArray.js
+++ AlgoB/leetcodeArray.js
@@ -1,16 +1,16 @@
-arr = [0, 1, 0, 3, 12];
+// arr = [0, 1, 0, 3, 12];
 
-function moveZero(arr) {
-  let index = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      let temp = arr[i];
-      arr[i] = arr[index];
-      arr[index] = temp;
-      index++;
-    }
-  }
-  return arr;
-}
+// function moveZero(arr) {
+//   let index = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       let temp = arr[i];
+//       arr[i] = arr[index];
+//       arr[index] = temp;
+//       index++;
+//     }
+//   }
+//   return arr;
+// }
 
-moveZer0(arr);
+// moveZer0(arr);
