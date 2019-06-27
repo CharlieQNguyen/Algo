00c2e0e73b8a85f2eaf93d056260f06f417c2814
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-06-27 12:00:05 -0700

    fizzBuzz

--- AlgoB/algo.js
+++ AlgoB/algo.js
@@ -162,3 +162,24 @@
 // }
 
 // swapString(arr);
+
+// num = 17;
+// function fizzBuzz(num) {
+//   let str = "";
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 == 0 && i % 5 == 0 && i == num) {
+//       str += " and FizzBuzz.";
+//     } else if (i % 3 == 0 && i % 5 == 0) {
+//       str += " FizzBuzz";
+//     } else if (i % 3 == 0) {
+//       str += " Fizz";
+//     } else if (i % 5 == 0) {
+//       str += " Buzz ";
+//     } else {
+//       str += " " + i;
+//     }
+//   }
+//   return str;
+// }
+
+// fizzBuzz(num);
