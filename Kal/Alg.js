61789d3d397ed1f733a1200abf8d00d8ebb54d41
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-07-11 12:26:46 -0700

    one dup

--- Kal/Alg.js
+++ Kal/Alg.js
@@ -56,15 +56,35 @@
 // singleEl(arr);
 
 
-arr = [-1, 1.5, 2, 2.5, 4, 5];
-num = -1;
+// arr = [-1, 1.5, 2, 2.5, 4, 5];
+// num = -1;
 
-function numberFinder(arr, num) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === num) {
-      return i;
-    }
-  }
-}
+// function numberFinder(arr, num) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === num) {
+//       return i;
+//     }
+//   }
+// }
 
-numberFinder(arr, num);
+// numberFinder(arr, num);
+
+// arr = [-1, 3, 4, 5, 7, 7, 8];
+
+// function oneDup(arr) {
+//   let hash = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!hash[arr[i]]) {
+//       hash[arr[i]] = 1;
+//     } else {
+//       hash[arr[i]] += 1;
+//     }
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (hash[arr[i]] > 1) {
+//       return arr[i];
+//     }
+//   }
+// }
+
+// oneDup(arr);
