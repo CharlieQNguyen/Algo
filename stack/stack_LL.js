6052ad06bce0da837d5770da41e565cd140792a2
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-08-21 00:21:25 -0700

    stack LL

---
+++ stack/stack_LL.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.head = null;
+    }
+    // push data onto the stack
+    push(data) {
+        // create newNode to add to stack
+        let newNode = new Node(data)
+        // newNode next points to the head
+        newNode.next = this.head
+        // move head to the newNode
+        this.head = newNode;
+    }
+    // pop the top node off the stack LIFO = Last In First Out
+    pop() {
+        // if there is nothing in the stack
+        if(this.head === null) {
+            return;
+        }
+        // normal case
+        //create temp call newHead
+        let newHead = this.head;
+        //move head to the next node
+        this.head = this.head.next;
+        //newHead pointer points to null to disconnect
+        newHead.next = null;
+        //return the node that was deleted stored in newHead variable 
+        return newHead.data
+    }
+
+    print() {
+        let pointer = this.head;
+
+        while(pointer) {
+            console.log(pointer.data);
+            pointer = pointer.next
+        }
+    }
+}
+
+stack1 = new Stack;
+stack1.push(100)
+stack1.push(200)
+stack1.push(300)
+stack1.pop();
+stack1.print();
