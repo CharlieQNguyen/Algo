30110763275386862f19fef4e701adf44a38af4d
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-08-27 13:36:14 -0700

    balance fixed

--- Kal/stackHW/Stack-1.0.js
+++ Kal/stackHW/Stack-1.0.js
@@ -1,22 +1,26 @@
 //Find missing parenthesis in a given expression – 2 * ( 3 + 5(sasdfasdfasd)
 
-str = "– 2 * ( 3 + 5(sasdfasdfasd))";
+str = "– 2 * ( 3 + 5(sasdfasdfasd)))))";
 
 function balance(str) {
   let stack = [];
+  let count = 0;
   for (let i = 0; i < str.length; i++) {
     if (str[i] === "(") {
       stack.push(str[i]);
     } else if (str[i] === ")") {
-      stack.pop();
+      if (stack.length !== 0) stack.pop();
+      else if (stack.length === 0) count++;
     }
   }
-  for (let i = 0; i < stack.length; i++) {
-    if (stack.length !== 0) {
-      return stack[i] + " needs a closing bracket";
-    }
+  
+  if(count < stack.length) {
+    return "need more ) ";
+  } else if(count > stack.length) {
+    return "need more ("
+  } else {
+    return "no braces needed"
   }
-  return "no missing";
 }
 
 balance(str);
