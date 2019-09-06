3c5f0899ca5ea7c624e161456d9d2f1bb7b20732
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-09-06 16:29:00 -0700

    string compressed

---
+++ Kal/stringHw/stringCompress.js
@@ -0,0 +1,21 @@
+//Implement a method to perform a basic string compression using the counts of repeated characters. For example, 
+//the string aabccccaaa would become a2b1c4a3. If the compressed string would not become smaller than the 
+//original string, your method should return the original string.
+
+str = "aabccccaaa";
+
+function strCompress(str) {
+  let str2 = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+      count++;
+    } else {
+      str2 += str[i] + String(count);
+      count = 1;
+    }
+  }
+  return str2;
+}
+
+strCompress(str);
