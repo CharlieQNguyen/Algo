b7f6df9d07ef39d86890573bd8b16b937267b1b6
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-09-09 14:18:27 -0700

    level 1 heap

---
+++ Kal/Heap/maxHead-1.0.js
@@ -0,0 +1,31 @@
+arr = [7, 2, 10, 5, 15];
+
+function maxHeap(arr) {
+  let arr2 = [];
+  //let currentIndex = 0;
+  let p;
+  for (let i = 0; i < arr.length; i++) {
+    let currentIndex = i;
+    arr2.push(arr[i]);
+    console.log(arr2);
+    while (currentIndex > 0) {
+      if (i % 2 === 0) {
+        p = (currentIndex - 2) / 2;
+      } else {
+        p = (currentIndex - 1) / 2;
+      }
+      if (arr2[p] < arr2[currentIndex]) {
+        let temp = arr[p];
+        arr2[p] = arr2[currentIndex];
+        arr2[currentIndex] = temp;
+        currentIndex = p;
+      } else {
+        break;
+      }
+      //currentIndex = p;
+    }
+  }
+  console.log(arr2);
+}
+
+maxHeap(arr);
