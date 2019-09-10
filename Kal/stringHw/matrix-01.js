219f664dca3f8a2fd55befadb4aee15974afe550
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-09-10 14:34:28 -0700

    matrix update

--- Kal/stringHw/matrix-01.js
+++ Kal/stringHw/matrix-01.js
@@ -15,3 +15,51 @@ function zeroMatrix(matrix) {
 }
 
 zeroMatrix(matrix);
+
+
+
+
+// matrix = [[2, 5, 1, 0], [1, 3, 3, 6], [0, 5, 1, 6]];
+
+// function zeroMatrix(matrix) {
+//   let rows0 = [];
+//   let cols0 = [];
+
+//   for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+//     const currArray = matrix[rowIndex];
+//     //console.log(currArray)
+//     for (let colIndex = 0; colIndex < currArray.length; colIndex++) {
+//       //console.log(currArray[colIndex])
+//       if (currArray[colIndex] === 0) {
+//         rows0.push(rowIndex);
+//         cols0.push(colIndex);
+//       }
+//     }
+//   }
+//   console.log(rows0, cols0);
+//   // for(let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+//   //   const currArray = matrix[rowIndex]
+//   //   for(let colIndex = 0; colIndex < currArray.length; colIndex++) {
+//   //     for(rowIndex in rows0) {
+//   //       matrix[rowIndex][colIndex] = 0;
+//   //     }
+//   //     for(colIndex in cols0) {
+//   //       matrix[rowIndex][colIndex] = 0;
+//   //     }
+//   //   }
+//   // }
+
+//   for (let i = 0; i < rows0.length; i++) {
+//     for (let j = 0; j < matrix[0].length; j++) {
+//       matrix[rows0[i]][j] = 0;
+//     }
+//   }
+//   for (let i = 0; i < cols0.length; i++) {
+//     for (let j = 0; j < matrix.length; j++) {
+//       matrix[j][cols0[i]] = 0;
+//     }
+//   }
+//   return matrix;
+// }
+
+// zeroMatrix(matrix);
