6caa71e13ee75f8d847cc66e11097fe128ba5fc4
Charlie Nguyen <charlie.nguyen43@gmail.com>
2019-10-15 11:41:07 -0700

    kadane 2

--- Kal/arr2/sumContiguousArr.js
+++ Kal/arr2/sumContiguousArr.js
@@ -23,3 +23,37 @@ function kadane(arr) {
   }
   return maxGlobal;
 }
+
+
+// arr = [1, 4, -20, 6, 10, -5];
+
+// function kadane(arr) {
+//   let maxCurrent = arr[0];
+//   let maxGlobal = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     console.log(arr[i] + " arr[i]");
+//     console.log(maxCurrent + " maxCurrent Before");
+//     if (arr[i] + maxCurrent > arr[i]) {
+//       maxCurrent += arr[i];
+//       console.log(maxCurrent + " maxCurrent after");
+//     } else if (arr[i] > maxCurrent) {
+//       maxCurrent = arr[i];
+//     }
+//     if (maxCurrent > maxGlobal) {
+//       console.log(maxGlobal + " Mglobal before");
+//       maxGlobal = maxCurrent;
+//       console.log(maxGlobal + " Mglobal after");
+//     }
+//     console.log("*****************");
+//     // console.log(maxCurrent + " maxCurrentBefore")
+//     // maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
+//     // console.log(arr[i] + " arr[i]")
+//     // console.log(maxCurrent + " maxCurretnAfter")
+//     // maxGlobal = Math.max(maxCurrent, maxGlobal);
+//     // console.log(maxGlobal + " Global")
+//     // console.log("**************")
+//   }
+//   return maxGlobal;
+// }
+
+// kadane(arr);
