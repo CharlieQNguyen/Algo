66e42cec4ac51e9313aaba56a56c3bad6a94a3b3
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-04-02 21:48:44 -0700

    findRange python

--- UW/findRange.js
+++ UW/findRange.js
@@ -24,3 +24,22 @@ function findRange(arr) {
 }
 
 findRange(arr);
+
+/*
+def find_range(listnum):
+    """
+    The function is aiming to find the range from the biggest and the smallest number
+    or when the both number are the same the range or be 1
+    """
+    max = listnum[0]
+    min = listnum[0]
+    for num in range(1,len(listnum)-1):
+        if max < listnum[num]:
+            max = listnum[num]
+        elif min > listnum[num]:
+            min = listnum[num]
+    if max != min:
+        return max - min +1
+    else:
+        return 1
+*/
