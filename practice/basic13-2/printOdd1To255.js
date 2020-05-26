b14b6d4c26de8e975bdb474d280ed04977d8a50d
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-25 17:46:42 -0700

    odd

---
+++ practice/basic13-2/printOdd1To255.js
@@ -0,0 +1,21 @@
+// function printOdd1To255() {
+//     for(let i = 0; i <= 255; i++) {
+//         if(i % 2 !== 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// printOdd1To255()
+
+function printOdd1To255() {
+    i = 0;
+    while(i <= 255) {
+        if(i % 2 !== 0) {
+            console.log(i)
+        }
+        i++;
+    }
+}
+
+printOdd1To255()
