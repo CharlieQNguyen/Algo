dc7e4f077e0dc50dde6ea0475393371e92da9e2e
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-03 17:31:28 -0700

    kal practice

---
+++ KalPractice/determineUique.js
@@ -0,0 +1,17 @@
+// str = "aaaaaabbbbbccccc"
+// str = "abcde"
+
+// function determineUique(str) {
+//     let hash = {};
+//     for(let i = 0; i < str.length; i++) {
+//         if(hash[str[i]]) {
+//             return console.log(false);
+//         } else {
+//             hash[str[i]] = 1;
+//         }
+//     }
+//     return console.log(true);
+// }
+
+// determineUique(str)
+
