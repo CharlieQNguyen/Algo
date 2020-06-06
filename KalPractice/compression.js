595816062b1a62b155a59b9dd6f3b4689dc03ebe
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-06 15:32:36 -0700

    compression

---
+++ KalPractice/compression.js
@@ -0,0 +1,19 @@
+str = "aabccccaaa";
+result = "a2b1c4a3";
+
+function compression(str) {
+    let str2 =""
+    let count = 1;
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] == str[i+1]) {
+            count++;
+        } else {
+            str2 += str[i];
+            str2 += count;
+            count = 1;
+        }
+    }
+    console.log(str2)
+}
+
+compression(str);
