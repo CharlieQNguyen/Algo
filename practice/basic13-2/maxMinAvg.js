a3f29e29867c42e35804bbb8b248fede9bfdbad7
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-06 12:56:48 -0700

    min max avg practice

---
+++ practice/basic13-2/maxMinAvg.js
@@ -0,0 +1,19 @@
+arr = [3, 1, -6, -2, 10, 30, 15];
+
+function maxMinAvg(arr) {
+    let max = -Infinity;
+    let min = Infinity;
+    let sum = arr[0];
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+        if(arr[i] > max) {
+            max = arr[i]
+        } else if(arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    let avg = sum/arr.length;
+    console.log("Max: " + max + " Min: " + min + " Avg: " + avg)
+}
+
+maxMinAvg(arr);
