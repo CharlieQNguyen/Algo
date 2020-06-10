f53038998716f9ca6503e974d586f8d38c0886aa
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-09 17:39:54 -0700

    zero matrix

--- KalPractice/matrixZero.js
+++ KalPractice/matrixZero.js
@@ -13,11 +13,22 @@ outcome = [
 function zeroMatrix(matrix) {
     let row = {};
     let col = {};
-    for(let i = 0; i < matrix.length; i++) {
-        for(let j = 0; j < matrix[i].length; j++) {
-            if(matrix[i][j] === 0) {
-                row
-            }
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+        if(matrix[i][j] === 0) {
+            row[i] = 1;
+            col[j] = 1;
         }
     }
+  }
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(i in row === true || j in col === true) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  console.log(matrix)
 }
+
+zeroMatrix(matrix);
