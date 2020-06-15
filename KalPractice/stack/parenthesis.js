9fb92c8e8a09f40d5b496b5e0c77967b7ff74c32
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-14 21:44:14 -0700

    param

--- KalPractice/stack/parenthesis.js
+++ KalPractice/stack/parenthesis.js
@@ -1,23 +1,34 @@
 // 1.Find missing parenthesis in a given expression – 2 * ( 3 + 5(sasdfasdfasd)
 
-str = "– 2 * ( 3 + 5(sasdfasdfasd)))))"
-str2 = "()[]{}";
-str3 = "{[]}"
-str4 = "([)]";
+// str = "– 2 * ( 3 + 5(sasdfasdfasd)))))"
+// str2 = "()[]{}";
+str2 = "{ [ ] }"
+// str2 = "([)]";
 
 function missingParenthesis(str2) {
     let stack = [];
-    let previousVal = [];
     for(let i = 0; i < str2.length; i++) {
         if(str2[i] === '(' || str2[i] === '[' || str2[i] === '{') {
-            stack.push(str2[i])
-            previousVal.push(str2[i]); 
-        } 
-        // else if(str2[i] === ')' || str2[i] === ']' || str2[i] === '}') {
-        //     stack.pop(str[i])
-        // }
+            stack.push(str2[i]);
+            var prev = str2[i];
+        } else if(stack.length !== 0 && str2[i] === ')' && prev === '(') {
+            stack.pop();
+            if(stack.length>0){
+                prev=stack.pop();
+                stack.push(prev);
+            } else {
+                prev = null;
+            }
+        } else if(stack.length !== 0 && str2[i] === ']' && prev === '[') {
+            stack.pop();
+        } else if(stack.length !== 0 && str2[i] === '}' && prev === '{') {
+            stack.pop()
+        }
     }
-    console.log(previousVal);
+    if(stack.length !== 0) {
+        return console.log(false)
+    }
+    return console.log(true)
 }
 
 missingParenthesis(str2);
