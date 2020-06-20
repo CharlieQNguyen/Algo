8fd5f9e94ceabd0cb8dc0f1b361ddda283ce6951
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-20 11:25:36 -0700

    num finder Binary Search

---
+++ KalPractice/array/numFinder.js
@@ -0,0 +1,21 @@
+arr = [-1, 1.5, 2, 2.5, 4, 5];
+
+num = 4;
+
+function numFinder(arr, num) {
+    let left = 0;
+    let right = arr.length-1;
+    while(left < right) {
+        var mid = Math.floor((left + right)/2);
+        if(arr[mid] == num) {
+            return console.log(arr[mid])
+        } else if(arr[mid] < num) {
+            left = mid + 1;
+        } else if(arr[mid] > num) {
+            right = mid - 1;
+        }
+    }
+    return console.log('no number')
+}
+
+numFinder(arr, num);
--- KalPractice/array/oneDup.js
+++ KalPractice/array/oneDup.js
@@ -1,17 +1,19 @@
 arr = [-1, 3, 4, 5, 7, 7, 8];
 
 
-function oneDup(arr) {
-    let hash = {};
-    for(let i = 0; i < arr.length; i++) {
-        if(hash[arr[i]]) {
-            return console.log(arr[i])
-        } else {
-            hash[arr[i]] = 1;
-        }
-    }
-    return console.log("no dup")
-}
+
+
+// function oneDup(arr) {
+//     let hash = {};
+//     for(let i = 0; i < arr.length; i++) {
+//         if(hash[arr[i]]) {
+//             return console.log(arr[i])
+//         } else {
+//             hash[arr[i]] = 1;
+//         }
+//     }
+//     return console.log("no dup")
+// }
 
 // function oneDup(arr) {
 //     for(let i = 0; i < arr.length; i++) {
