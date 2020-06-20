f1f86f8b593de27ecb4f6b351e334f34a7e22b1f
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-19 18:37:25 -0700

    binary

--- KalPractice/array/singleElement.js
+++ KalPractice/array/singleElement.js
@@ -1,7 +1,34 @@
 // given a sorted array with duplicates find the number that is not a duplicates
 
-arr = [1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8];
+//arr = [1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8];
+arr = [1, 1, 2, 2, 3, 3, 4, 4, 8, 8, 9]
 
+function singleEl(arr) {
+        if(arr.length %2 === 0) {
+            return console.log("no singles")
+        }
+        let start = 0;
+        let end = arr.length-1;
+        console.log(end)
+        while(start < end) {
+            var mid = Math.floor((start+end)/2)
+            var isEven = (end - mid) % 2 === 0; 
+        if(arr[mid] == arr[mid - 1]) {
+            if(isEven) {
+                end = mid - 2;
+            } else {
+                start = mid + 1;
+            }
+        } else if(arr[mid] == arr[mid + 1]) {
+            if(isEven) {
+                start = mid + 2
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return console.log(arr[start]);
+}
 
 
 
