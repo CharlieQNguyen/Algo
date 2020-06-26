7ff8d1afdcf87799adf4cdbacb4e34664160a8a7
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-25 17:21:33 -0700

    find missing num

--- KalPractice/array2/findMissingNum.js
+++ KalPractice/array2/findMissingNum.js
@@ -12,15 +12,39 @@ Output : 7
 
 */
 
-arr = [1, 2, 3, 4, 5, 6, 8, 9];
+//arr = [1,2,3,4,5,6,7,9]  // l = 8     [1,2,4,5,6,7,8,9]  // l = 8
+    // 0,1,2,3,4,5,6,7                 0,1,2,3,4,5,6,7                                              
+    // mid = 3; arr[3] = 4             mid = 3; arr[3] = 5
+    // --> start = 3 count = -1
 
-function missingNum(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== i + 1) {
-            return console.log(i + 1)
-        }
+    // [1,2,3,4,5,6,7,9]  l = 8
+    //  0,1,2,3,4,5,6,7      
+    // mid = 5; arr[5] = 6
+    // start --> 5 count = 3
+
+    // [1,2,3,4,5,6,7,9]  l = 8
+    //  0,1,2,3,4,5,6,7      
+    // mid = 6; arr[6] = 7
+    // start --> 6 count =5
+
+
+
+function missingNumBinarySearch(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let count = -1;
+  while (start < end) {
+    var mid = Math.floor((start + end) / 2);
+    if (arr[mid] == mid + 1 && count != start) {
+      count = start;
+      start = mid;
+    } else if (arr[mid] == mid + 2 && count != end) {
+      count = end;
+      end = mid;
+    } else {
+      return count + 2;
     }
-    return console.log('no missing')
+  }
 }
 
-missingNum(arr);
+missingNumBinarySearch(arr);
