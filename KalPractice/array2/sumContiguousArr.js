6766fd5c19be2b0eebef193c2315da04b6a5457f
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-07-07 18:56:04 -0700

    Kadane

--- KalPractice/array2/sumContiguousArr.js
+++ KalPractice/array2/sumContiguousArr.js
@@ -3,19 +3,42 @@
 output = 7
 4 + (-1) + (-2) + 1 + 5 = 7
 
+[-2, 3, 2, -1]
+
 */
 
-arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+//arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+arr = [-2, 3, -5, -1];
 
 function sumContiguousSubArr(arr) {
-    let end = arr.length-1;
-    let sum = 0;
-    for(start = 0; start < arr.length; start++) {
-        if(arr[start] < 0 && arr[end] < 0) {
-            start++;
-            end--;
-        } 
+    let globalMax = 0; // 3
+    let currentMax = 0; // 3, 0
+    for(let i = 0; i < arr.length; i++) {
+        currentMax = currentMax + arr[i]; // 3, -1, -1
+        if(globalMax < currentMax) {
+            globalMax = currentMax
+        }
+        if(currentMax < 0) { 
+            currentMax = 0; // 0, 0, 0
+        }
     }
+    return console.log(globalMax)
 }
+
+// arr = [-2, 3, 2, -1];
+
+// function sumContiguousSubArr(arr) {
+//     let currentMax = arr[0]; // -2, 3, 5
+//     let globalMax = arr[0]; //-2, 3, 5,
+//     for(let i = 1; i < arr.length; i++) {
+//         let prevCombineCurrentMax = currentMax + arr[i] // 1, 5, 4
+//         currentMax = Math.max(arr[i], prevCombineCurrentMax); // 3, 5, 4
+//         if(currentMax > globalMax) {
+//             globalMax = currentMax // 3, 5
+//         }
+//     }
+//     return console.log(globalMax)
+// }
 
 sumContiguousSubArr(arr);
