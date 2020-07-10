da7785cd49a57ee2018d1f77331d8d2f59b61eda
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-07-10 16:57:50 -0700

    congtiguous review

--- KalPractice/array2/sizeKsumContigArr.js
+++ KalPractice/array2/sizeKsumContigArr.js
@@ -13,33 +13,50 @@ Explanation: Subarray with maximum sum is [3, 4].
 
 */
 
+arr =   [2, 3, 4, 1, 5];
+ //     [0, 1, 2, 3, 4];
+k = 2;
+
+function sumContiguousSubArrSizeK(arr, k) {
+    let maxSum = 0; 
+    let windowSum = 0; 
+    let start = 0; 
+    for(end = 0; end < arr.length; end++) {
+        windowSum += arr[end]; 
+        if(end >= k - 1) {
+            maxSum = Math.max(maxSum, windowSum);
+            windowSum -= arr[start]
+            start += 1;
+        }
+    }
+    return console.log(maxSum)
+}
+
+sumContiguousSubArrSizeK(arr, k);
 
 
 
 
-
-
-
-arr = [2, 1, 5, 1, 3, 2];
-k = 3;
+// arr = [2, 1, 5, 1, 3, 2];
+// k = 3;
 
 /* [2, 1, 5, 1, 3, 2]
    [0, 1, 2, 3, 4, 5]
 */
 
-function sumContiguousSubArrSizeK(arr, k) {
-    let windowSum = 0; // current window sum
-    let maxSum = 0; // need max sum to keep track of the max in the current window
-    let start = 0; // only move the start after our end is bigger than size K
-    for(let end = 0; end < arr.length; end++) {
-        windowSum += arr[end]; // adding to the window
-        if(end >= k - 1) { // if end is greater than K, it's time to evaluate. (reason for K - 1 because of end index starting at 0)
-            maxSum = Math.max(windowSum, maxSum) // compare the current maxSum vs current windowSum
-            windowSum -= arr[start]; // subtracting the start of the window from the window sum
-            start +=1 // moving the index of the start (moving the window)
-        }
-    }
-    console.log(maxSum)   
-}
+// function sumContiguousSubArrSizeK(arr, k) {
+//     let windowSum = 0; // current window sum
+//     let maxSum = 0; // need max sum to keep track of the max in the current window
+//     let start = 0; // only move the start after our end is bigger than size K
+//     for(let end = 0; end < arr.length; end++) {
+//         windowSum += arr[end]; // adding to the window
+//         if(end >= k - 1) { // if end is greater than K, it's time to evaluate. (reason for K - 1 because of end index starting at 0)
+//             maxSum = Math.max(windowSum, maxSum) // compare the current maxSum vs current windowSum
+//             windowSum -= arr[start]; // subtracting the start of the window from the window sum
+//             start +=1 // moving the index of the start (moving the window)
+//         }
+//     }
+//     console.log(maxSum)   
+// }
 
-sumContiguousSubArrSizeK(arr, k);
+// sumContiguousSubArrSizeK(arr, k);
