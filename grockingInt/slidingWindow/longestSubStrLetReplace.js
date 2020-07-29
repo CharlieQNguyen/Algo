0da626d59e735ba41e7fae5bdf94b1e44cd45aee
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-07-28 18:43:24 -0700

    longest sub replace

---
+++ grockingInt/slidingWindow/longestSubStrLetReplace.js
@@ -0,0 +1,79 @@
+/*
+
+Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
+
+Example 1:
+
+Input: String="aabccbb", k=2
+Output: 5
+Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
+Example 2:
+
+Input: String="abbcb", k=1
+Output: 4
+Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
+Example 3:
+
+Input: String="abccde", k=1
+Output: 3
+Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
+*/
+
+str = "abccde";
+k = 1;
+
+function length_of_longest_substring(str, k) {
+  let windowStart = 0,
+    maxLength = 0,
+    maxRepeatLetterCount = 0,
+    frequencyMap = {};
+
+  // Try to extend the range [windowStart, windowEnd]
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    console.log(str);
+    console.log("* WindowStart is at index " + windowStart + "  ******");
+    console.log("* WindowEnd is at index " + windowEnd + "  ******");
+    const rightChar = str[windowEnd];
+    if (!(rightChar in frequencyMap)) {
+      console.log("I am adding a key to my hashmap " + rightChar);
+      frequencyMap[rightChar] = 0;
+    }
+    frequencyMap[rightChar] += 1;
+    maxRepeatLetterCount = Math.max(
+      maxRepeatLetterCount,
+      frequencyMap[rightChar]
+    );
+
+    // Current window size is from windowStart to windowEnd, overall we have a letter which is
+    // repeating 'maxRepeatLetterCount' times, this means we can have a window which has one letter
+    // repeating 'maxRepeatLetterCount' times and the remaining letters we should replace.
+    // if the remaining letters are more than 'k', it is the time to shrink the window as we
+    // are not allowed to replace more than 'k' letters
+    console.log("my max letter count is " + maxRepeatLetterCount);
+    console.log("The WINDOW length is " + (windowEnd - windowStart + 1));
+    console.log(
+      "is THIS: " +
+        (windowEnd - windowStart + 1 - maxRepeatLetterCount) +
+        " bigger than our k: " +
+        k +
+        " ???"
+    );
+    if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+      console.log("YES!!!");
+      leftChar = str[windowStart];
+      frequencyMap[leftChar] -= 1;
+      windowStart += 1;
+      console.log("I have nudged the WindowStart to index " + windowStart);
+    }
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    console.log("* I just ended step " + (windowEnd + 1) + "  ******");
+    console.log("**************************");
+  }
+  return maxLength;
+}
+
+console.log(length_of_longest_substring("aabccbb", 2));
--- grockingInt/slidingWindow/minSizeSubSum.js
+++ grockingInt/slidingWindow/minSizeSubSum.js
@@ -5,28 +5,30 @@
 arr = [2,3,1,2,4,3]
 s = 7
 
-function minSizeSubArray(arr, s) {
-    let windowStart = 0;
-    let windowSum = 0;
-    let minLength = Infinity;
-    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-        windowSum += arr[windowEnd]; // expand
-        // while windowSum is bigger than S keep shrinking until it'not bigger or equal to s
-        while(windowSum >= s) {
-            // get minimum length by comparing with actual window
-            // actual window = end - start + 1
-            minLength = Math.min(minLength, windowEnd - windowStart + 1);
-            // move windowStart by subtracting one value from the actual windowSum
-            windowSum -= arr[windowStart]
-            // move the index of window start
-            windowStart += 1
-        }
-    }
-    if(minLength === Infinity) {
-        return 0;
-    }
-    console.log(minLength)
-    return minLength
-}
+function 
+
+// function minSizeSubArray(arr, s) {
+//     let windowStart = 0;
+//     let windowSum = 0;
+//     let minLength = Infinity;
+//     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+//         windowSum += arr[windowEnd]; // expand
+//         // while windowSum is bigger than S keep shrinking until it'not bigger or equal to s
+//         while(windowSum >= s) {
+//             // get minimum length by comparing with actual window
+//             // actual window = end - start + 1
+//             minLength = Math.min(minLength, windowEnd - windowStart + 1);
+//             // move windowStart by subtracting one value from the actual windowSum
+//             windowSum -= arr[windowStart]
+//             // move the index of window start
+//             windowStart += 1
+//         }
+//     }
+//     if(minLength === Infinity) {
+//         return 0;
+//     }
+//     console.log(minLength)
+//     return minLength
+// }
 
 minSizeSubArray(arr, s);
