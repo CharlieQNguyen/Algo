c3b711b7d8235f5acbcb287ff50bc6c0f5dd9cc9
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-08-14 14:50:46 -0700

    max sum sub array

--- grockingInt/slidingWindow/maxSumSubArray.js
+++ grockingInt/slidingWindow/maxSumSubArray.js
@@ -45,3 +45,21 @@ maxSumSubArray(arr, k);
 //   return console.log(maxSum);
 // }
 // maxSumSubSizeK(arr, k);
+
+function maximum_Sum_Subarray_SizeK(arr, k) {
+  let windowStart = 0;
+  let maxSum = 0;
+  let windowSum = 0;
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    let windowLength = windowEnd - windowStart + 1;
+    if (windowLength === k) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
+  }
+  return console.log(maxSum);
+}
+
+maximum_Sum_Subarray_SizeK(arr, k);
--- grockingInt/slidingWindow/minSizeSubSum.js
+++ grockingInt/slidingWindow/minSizeSubSum.js
@@ -5,30 +5,93 @@
 arr = [2,3,1,2,4,3]
 s = 7
 
-function 
+ 
 
-// function minSizeSubArray(arr, s) {
-//     let windowStart = 0;
-//     let windowSum = 0;
-//     let minLength = Infinity;
-//     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-//         windowSum += arr[windowEnd]; // expand
-//         // while windowSum is bigger than S keep shrinking until it'not bigger or equal to s
-//         while(windowSum >= s) {
-//             // get minimum length by comparing with actual window
-//             // actual window = end - start + 1
-//             minLength = Math.min(minLength, windowEnd - windowStart + 1);
-//             // move windowStart by subtracting one value from the actual windowSum
-//             windowSum -= arr[windowStart]
-//             // move the index of window start
-//             windowStart += 1
-//         }
+function minSizeSubArray(arr, s) {
+    let windowStart = 0;
+    let windowSum = 0;
+    let minLength = Infinity;
+    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        windowSum += arr[windowEnd]; // expand
+        // while windowSum is bigger than S keep shrinking until it'not bigger or equal to s
+        while(windowSum >= s) {
+            // get minimum length by comparing with actual window
+            // actual window = end - start + 1
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            // move windowStart by subtracting one value from the actual windowSum
+            windowSum -= arr[windowStart]
+            // move the index of window start
+            windowStart += 1
+        }
+    }
+    if(minLength === Infinity) {
+        return 0;
+    }
+    console.log(minLength)
+    return minLength
+}
+
+minSizeSubArray(arr, s);function SmallSubSum(arr, s){
+	let start = 0;
+    let end = 0;
+    let sum = 0;
+    let minLength = arr.length;
+    for(let i = 0; i<arr.length; i++){
+    	sum += arr[i];
+        if(sum >= s){
+          	end = i;
+            if (minLength > end - start+1){
+            minLength = end - start+1;
+            start = end;
+          	}
+          	sum = arr[i];
+    	}
+      	
+	}
+  return arr.slice(start,end+1)
+}
+
+// function SmallSubSum(arr, s) {
+//   let start = 0;
+//   let end = 0;
+//   let sum = 0;
+//   let minLength = arr.length;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     if (sum >= s) {
+//       end = i;
+//       if (minLength > end - start + 1) {
+//         minLength = end - start + 1;
+//         start = end;
+//       }
+//       sum = arr[i];
 //     }
-//     if(minLength === Infinity) {
-//         return 0;
-//     }
-//     console.log(minLength)
-//     return minLength
+//   }
+//   return arr.slice(start, end + 1);
 // }
 
-minSizeSubArray(arr, s);
+arr = [2, 1, 5, 2, 3, 2];
+s = 7;
+
+function smallestSubSum(arr, s) {
+  let windowStart = 0;
+  let minLength = arr.length;
+  let windowSum = 0;
+
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+
+    while (windowSum >= s) {
+      let windowLength = windowEnd - windowStart + 1;
+      minLength = Math.min(minLength, windowLength);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
+  }
+  if (minLength === 0) {
+    return console.log(0);
+  }
+  return console.log(minLength);
+}
+
+smallestSubSum(arr, s);
