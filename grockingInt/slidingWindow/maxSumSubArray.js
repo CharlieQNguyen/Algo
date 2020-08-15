826e027782e6cced94d60f8de178e47d405dc90b
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-08-15 10:14:08 -0700

    window

--- grockingInt/slidingWindow/maxSumSubArray.js
+++ grockingInt/slidingWindow/maxSumSubArray.js
@@ -1,29 +1,50 @@
-arr = [2, 1, 5, 1, 3, 2];
-k = 3
+// arr = [2, 1, 5, 1, 3, 2];
+// k = 3
 
-function maxSumSubArray(arr, k) {
-    let maxSum = 0;
-    let windowSum = 0;
-    let windowStart = 0;
+arr = [2, 3, 4, 1, 5];
+k = 2;
 
-    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-        // keep adding to the sum
-        windowSum += arr[windowEnd];
-        // now use sliding window if we do not hit requirement size of K
-        if(windowEnd >= k - 1) {
-            // pick the max between the sums
-            maxSum = Math.max(maxSum, windowSum)
-            // subtract element coming out
-            windowSum = windowSum - arr[windowStart]
-            // sliding window ahead
-            windowStart += 1  
-        }
+function maxSumK(arr, k) {
+  let start = 0;
+  let maxSum = 0;
+  let windowSum = 0;
+
+  for(let end = 0; end < arr.length; end++) {
+    windowSum += arr[end];
+    let windowLength = end - start + 1;
+    if(windowLength === k) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[start];
+      start++;
     }
-    console.log(maxSum)
-    return maxSum
+  }
+  return console.log(maxSum)
 }
+maxSumK(arr, k);
 
-maxSumSubArray(arr, k);
+// function maxSumSubArray(arr, k) {
+//     let maxSum = 0;
+//     let windowSum = 0;
+//     let windowStart = 0;
+
+//     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+//         // keep adding to the sum
+//         windowSum += arr[windowEnd];
+//         // now use sliding window if we do not hit requirement size of K
+//         if(windowEnd >= k - 1) {
+//             // pick the max between the sums
+//             maxSum = Math.max(maxSum, windowSum)
+//             // subtract element coming out
+//             windowSum = windowSum - arr[windowStart]
+//             // sliding window ahead
+//             windowStart += 1  
+//         }
+//     }
+//     console.log(maxSum)
+//     return maxSum
+// }
+
+// maxSumSubArray(arr, k);
 
 // arr = [2, 3, 4, 1, 5];
 // k = 2;
@@ -46,20 +67,20 @@ maxSumSubArray(arr, k);
 // }
 // maxSumSubSizeK(arr, k);
 
-function maximum_Sum_Subarray_SizeK(arr, k) {
-  let windowStart = 0;
-  let maxSum = 0;
-  let windowSum = 0;
-  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    windowSum += arr[windowEnd];
-    let windowLength = windowEnd - windowStart + 1;
-    if (windowLength === k) {
-      maxSum = Math.max(maxSum, windowSum);
-      windowSum -= arr[windowStart];
-      windowStart++;
-    }
-  }
-  return console.log(maxSum);
-}
+// function maximum_Sum_Subarray_SizeK(arr, k) {
+//   let windowStart = 0;
+//   let maxSum = 0;
+//   let windowSum = 0;
+//   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+//     windowSum += arr[windowEnd];
+//     let windowLength = windowEnd - windowStart + 1;
+//     if (windowLength === k) {
+//       maxSum = Math.max(maxSum, windowSum);
+//       windowSum -= arr[windowStart];
+//       windowStart++;
+//     }
+//   }
+//   return console.log(maxSum);
+// }
 
-maximum_Sum_Subarray_SizeK(arr, k);
+// maximum_Sum_Subarray_SizeK(arr, k);
---
+++ grockingInt/slidingWindow/smallestWindowSubStr.js
@@ -0,0 +1,74 @@
+/*
+
+Given a string and a pattern, find the smallest substring in the given string which has all the characters of the given pattern.
+
+Example 1:
+
+Input: String="aabdec", Pattern="abc"
+Output: "abdec"
+Explanation: The smallest substring having all characters of the pattern is "abdec"
+
+Input: String="abdabca", Pattern="abc"
+Output: "abc"
+Explanation: The smallest substring having all characters of the pattern is "abc".
+
+Input: String="adcad", Pattern="abc"
+Output: ""
+Explanation: No substring in the given string has all characters of the pattern.
+
+*/
+
+function find_substring(str, pattern) {
+  let windowStart = 0,
+    matched = 0,
+    substrStart = 0,
+    minLength = str.length + 1,
+    charFrequency = {};
+
+  for (i = 0; i < pattern.length; i++) {
+    const chr = pattern[i];
+    if (!(chr in charFrequency)) {
+      charFrequency[chr] = 0;
+    }
+    charFrequency[chr] += 1;
+  }
+
+  // try to extend the range [windowStart, windowEnd]
+  for (windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (rightChar in charFrequency) {
+      charFrequency[rightChar] -= 1;
+      if (charFrequency[rightChar] >= 0) {
+        // Count every matching of a character
+        matched += 1;
+      }
+    }
+
+    // Shrink the window if we can, finish as soon as we remove a matched character
+
+    (String = "aabdec"), (Pattern = "abc");
+    Output: "abdec";
+    while (matched === pattern.length) {
+      if (minLength > windowEnd - windowStart + 1) {
+        minLength = windowEnd - windowStart + 1;
+        substrStart = windowStart;
+      }
+
+      const leftChar = str[windowStart];
+      windowStart += 1;
+      if (leftChar in charFrequency) {
+        // Note that we could have redundant matching characters, therefore we'll decrement the
+        // matched count only when a useful occurrence of a matched character is going out of the window
+        if (charFrequency[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFrequency[leftChar] += 1;
+      }
+    }
+  }
+
+  if (minLength > str.length) {
+    return "";
+  }
+  return str.substring(substrStart, substrStart + minLength);
+}
